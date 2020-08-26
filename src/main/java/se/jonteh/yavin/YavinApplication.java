e80f958c09c4e0e5b338349fe2e9a20b9673b2ee
Jonathan Helsing <jonathan.helsing@gmail.com>
2020-08-26 20:19:10 +0200

    Add basic Spring boot web support

---
+++ src/main/java/se/jonteh/yavin/YavinApplication.java
@@ -0,0 +1,13 @@
+package se.jonteh.yavin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YavinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YavinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jonteh/yavin/YavinApplicationTest.java
@@ -0,0 +1,12 @@
+package se.jonteh.yavin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class YavinApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
