b7b4949db01c2b381041eabaa004c1496c95c8a1
Jonathan Helsing <jonathan.helsing@gmail.com>
2020-09-20 19:59:42 +0200

    Implemented the various existing API endpoints

--- src/main/java/se/jonteh/yavin/error/GlobalControllerExceptionHandler.java
+++ src/main/java/se/jonteh/yavin/error/GlobalControllerExceptionHandler.java
@@ -5,16 +5,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import se.jonteh.yavin.error.client.BadRequestException;
+import se.jonteh.yavin.error.client.ForbiddenException;
+import se.jonteh.yavin.error.client.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
+  @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
+  @ExceptionHandler(BadRequestException.class)
+  public HttpErrorException handleBadRequest(HttpServletRequest req, Exception e) {
+    if (e instanceof BadRequestException)
+      return (BadRequestException) e;
+    return new HttpErrorException("", "");
+  }
+
   @ResponseStatus(HttpStatus.NOT_FOUND) // 404
-  @ExceptionHandler(SnippetNotFound.class)
-  public HttpError handleNotFound(HttpServletRequest req, Exception e) {
-    if (e instanceof SnippetNotFound)
-      return (SnippetNotFound) e;
-    return new HttpError("", "");
+  @ExceptionHandler(NotFoundException.class)
+  public HttpErrorException handleNotFound(HttpServletRequest req, Exception e) {
+    if (e instanceof NotFoundException)
+      return (NotFoundException) e;
+    return new HttpErrorException("", "");
+  }
+
+  @ResponseStatus(HttpStatus.FORBIDDEN) // 403
+  @ExceptionHandler(ForbiddenException.class)
+  public HttpErrorException handleForbiddenCaller(HttpServletRequest req, Exception e) {
+    if (e instanceof ForbiddenException)
+      return (ForbiddenException) e;
+    return new HttpErrorException("", "");
   }
 
 }
---
+++ src/main/java/se/jonteh/yavin/error/client/ForbiddenException.java
@@ -0,0 +1,7 @@
+package se.jonteh.yavin.error.client;
+
+import se.jonteh.yavin.error.HttpErrorException;
+
+public class ForbiddenException extends HttpErrorException {
+  // TODO populate this class
+}
